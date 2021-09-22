f9e2added949da3a40ea9902d69115ef39c79ab3
Vithushan <[valid-email]>
2021-09-21 23:27:03 -0400

    worked on css

--- math.js
+++ math.js
@@ -1,15 +1,89 @@
 // To generate random numbers
 
-
-
 function random() {
+  console.log('randomizing numbers');
+  // For number 1
+  let x = Math.random() * 100;
+  let y = Math.round(x);
+  document.getElementById('num1').innerHTML = y;
+
+  let a = Math.random() * 100;
+  let b = Math.round(a);
+  document.getElementById('num2').innerHTML = b;
+
+  // Clearing current answer
+  document.getElementById('answer').value = '';
+}
+
+// to check if answer is correct
+
+// first, submit answer
+
+function submit() {
+  console.log('submitted answer');
+  let a = document.getElementById('answer').value;
+  document.getElementById('urAnswer').innerHTML = a;
+}
+
+function verify() {
+  console.log('Correct answer must be same as one submitted!');
+  let x = document.getElementById('num1').innerHTML;
+  let y = document.getElementById('num2').innerHTML;
+  let z = parseInt(x) + parseInt(y);
+  console.log(z);
+  document.getElementById('correctAnswer').innerHTML = z;
+}
+
+// timer
+
+
+
+// Subtraction
+
+function verify2() {
+  console.log('Correct answer must be same as one submitted!');
+  let x = document.getElementById('num1').innerHTML;
+  let y = document.getElementById('num2').innerHTML;
+  let z = parseInt(x) - parseInt(y);
+  console.log(z);
+  document.getElementById('correctAnswer').innerHTML = z;
+}
+
+// Multiply
+
+function random3() {
+  console.log('randomizing numbers');
+  // For number 1
+  let x = Math.random() * 10;
+  let y = Math.round(x);
+  document.getElementById('num1').innerHTML = y;
+
+  let a = Math.random() * 10;
+  let b = Math.round(a);
+  document.getElementById('num2').innerHTML = b;
+
+  // Clearing current answer
+  document.getElementById('answer').value = '';
+}
+
+function verify3() {
+  console.log('Correct answer must be same as one submitted!');
+  let x = document.getElementById('num1').innerHTML;
+  let y = document.getElementById('num2').innerHTML;
+  let z = parseInt(x) * parseInt(y);
+  console.log(z);
+  document.getElementById('correctAnswer').innerHTML = z;
+}
+
+// Divide
+function random4() {
     console.log('randomizing numbers')
     // For number 1 
-    let x = Math.random()*100
+    let x = Math.random()*10
     let y = Math.round(x)
     document.getElementById('num1').innerHTML=y
 
-    let a = Math.random()*100
+    let a = Math.random()*10
     let b = Math.round(a)
     document.getElementById('num2').innerHTML=b;
 
@@ -17,25 +91,12 @@ function random() {
     document.getElementById('answer').value=""
 }
 
-
-// to check if answer is correct 
-
-// first, submit answer
-
-function submit (){
-    console.log('submitted answer')
-   let a = document.getElementById('answer').value 
-   document.getElementById('urAnswer').innerHTML = a
-}
-
-
-function verify (){
-    console.log('Correct answer must be same as one submitted!')
-   let x = document.getElementById('num1').innerHTML
-   let y = document.getElementById('num2').innerHTML
-   let z = parseInt(x) + parseInt(y)
-   console.log(z)
-   document.getElementById('correctAnswer').innerHTML = z
-}
-
-// timer 
+function verify4() {
+    console.log('Correct answer must be same as one submitted!');
+    let x = document.getElementById('num1').innerHTML;
+    let y = document.getElementById('num2').innerHTML;
+    let z = parseInt(x) / parseInt(y);
+    console.log(z);
+    document.getElementById('correctAnswer').innerHTML = z;
+  }
+  
